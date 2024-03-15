09d180e7563832869de54d5e60557d2bd297d2cf
Carlos Veizaga <carlosveizaga.jcvc@gmail.com>
2024-03-14 22:54:24 -0400

    feat(auth): Add auth module.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
+
+
+// <?php
+
+// namespace App\Http\Controllers;
+
+// abstract class Controller
+// {
+//     //
+// }
