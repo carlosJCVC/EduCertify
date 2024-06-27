1de1e679ee9d9428a14aea13a898e41f7bb52c9a
Carlos Veizaga <carlosveizaga.jcvc@gmail.com>
2024-06-27 14:39:15 -0400

    fix(home page): Redirect home page to login.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('login'));
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('home');
